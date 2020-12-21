b4762f2019bb6b0300fef4d21a7b0ee917e2a0db
Rhys Shaw <45737158+RhysAlfShaw@users.noreply.github.com>
2020-12-21 15:06:43 +0000

    addition of project files

---
+++ FInal_Year_Project_Bristol/Morphology and error code.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 10:45:13 2020
+
+@author: rs17612
+"""
+
+from astropy.io import fits
+from astropy.table import Table
+from matplotlib.colors import LogNorm
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+class morph_statistic:
+    
+    def extract_fits(fitsfile,N):
+        hdu_list = fits.open(fitsfile) #make sure you look at directry
+        
+        hdu_list.info()                #this gives you ta types in the file you will need the primary 
+
+        evt_data = Table(hdu_list[1].data)
+        x = evt_data['x']
+        y = evt_data['y']
+        NBINS = (N,N)
+
+        A = np.histogram2d(x,y,NBINS)[0]
+        #print(A)
+        #plt.imshow(A,norm=LogNorm())
+        return A
+
+    def cluster_symetry(A,N):
+        n0 = 0
+        Atot = 0
+        for i in range(0,len(A)-1):
+            for j in range(0,len(A)-1):
+                if A[i,j] != 0:
+                    n0 = 1 + n0
+                    Atot =Atot + A[i,j]
+    
+        cpx = cpy = N/2
+        cp = np.array([[cpx],[cpy]])
+        res = 0
+        for i in range(0,N-1):
+            for j in range(0,N-1):
+                if i == cpx:
+                    if j ==cpy:
+                        res = res + 0
+                else:
+                    point = np.array([[i],[j]])
+                    x = np.subtract(point,cp)
+                    y = A[i,j]
+                    res = np.add(res,x*y)  
+    
+        meanA = Atot/n0
+        vec = res/(meanA*n0)
+    #print('Mean number of counts: ',meanA)     
+        c = np.linalg.norm(res)
+        sym = c/(meanA*n0)
+    
+    #print('Normalised vector:     ',c)
+    #print('Symetry value:         ',c/(meanA*n0))
+
+        return sym, c, vec
+
+
+    def sym_error(A,N,Ni):
+        
+        disym = []
+        for n in range(0,Ni):
+        
+            print(n)
+            An = np.zeros((len(A),len(A)))
+            for i in range(0,len(A)-1):
+                for j in range(0,len(A)-1):
+                    if A[i,j] != 0:
+                        #print(A[i,j])
+                        An[i,j] = np.random.poisson(A[i,j])
+            syme = morph_statistic.cluster_symetry(An,N)[0]
+            disym.append(syme)
+        #return di
+        
+        Mean = np.mean(disym)
+        sd = np.std(disym)
+        #fig, ax = plt.subplots()
+        #n, bins, patches = ax.hist(disym, 100, density=1)
+        #ax.set_xlabel('Morphology Metric')
+        #ax.set_ylabel('Frequecy')
+        #plt.show()
+        
+        return Mean, sd
+        
+
+fitsfile = np.array([['13517_remorph.fits',62]])
+N = int(fitsfile[0,1])*2
+A = morph_statistic.extract_fits(fitsfile[0,0], N)
+morph_stat, norm ,vec = morph_statistic.cluster_symetry(A,N)
+Mean, sd = morph_statistic.sym_error(A,N, 10000)
+print('Morph stat: ',morph_stat)
+print('Mean :',Mean)
+print('Sd :',sd)
+
---
+++ FInal_Year_Project_Bristol/Rcalculator.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 20:15:17 2019
+
+@author: rhysc
+
+This code was created for use in my Final Year Project, Where it would caclcuate the size of R500 given kT(clusters ICM temperature) 
+and its redshift. Its a GUI made with tkinter to help the work flow when analyseing cluster observations. 
+It is essentially a custom GUI calculator.
+
+"""
+#import cosmolopy as cos
+from astropy.cosmology import WMAP9 as cosmo
+import numpy as np
+import tkinter as tk
+from tkinter import ttk , Entry, Label, Tk, Button
+
+G = 4.30091E-3 #pc*Mo**(-1)(km/s)**2
+#k = 8.617333262145E−5 #ev*K**-1
+#kT = 14.4626 #K change for each iteration
+#z = 0.596 #needs to change for each cluster
+h = 0.696   
+ho = 69.6   
+alpha = 1.53 
+mo = 3.02E14  
+wm = 0.286  #omega matter
+wv = 0.714  #omega delta        
+
+
+class GUI:
+    def __init__(self,master):
+        self.master = master
+        master.title("Calculator For R_500!")
+        
+        vcmd = master.register(self.validate)
+        
+        self.label = Label(master,text='welcome to this caluculator for R_500, Please enter values in the correct entries.')
+        self.label.pack()
+        
+        self.zlabel = Label(master,text = 'Please enter the clusters redshift here:')
+        self.zlabel.pack()
+        self.z = Entry(master, validate='key', validatecommand=(vcmd,'%P'))
+        self.z.pack()
+        
+        self.kTlabel = Label(master, text='Please enter the Clusters Temperture in (KeV):')
+        self.kTlabel.pack()
+        
+        self.kT = Entry(master, validate='key',validatecommand=(vcmd,'%P'))
+        self.kT.pack()
+        
+        self.calc = Button(master, text='---calculate---', command=lambda:self.command(master,'calculate'))
+        self.calc.pack()
+        
+        self.clear = Button(master, text='--clear--', command=lambda:self.command(master,'clear'))
+        self.clear.pack()
+     
+    def command(self,master,method):
+        self.master = master
+        if method =='calculate':
+            
+            kT = float(self.kT.get())   #takes kT (temperature in units of energy) of cluster
+            z = float(self.z.get())     #takes redshift of cluster
+            scale = cosmo.kpc_proper_per_arcmin(z)/60*10**(-3)
+            
+            hz = ho*(0.3*(1+z)**3 + 0.7)**0.5
+            #hz = ho*(1+z)**(3/2)
+            E = hz/ho
+            
+            M_500 = mo*(kT/5)**alpha * E**(-1)*(h)**(-1)
+
+            pc_z = 3*(hz)**2/(8*np.pi*G*1E-6)
+            
+            R_500 = (3*M_500/(4*np.pi*500*pc_z))**(1/3)  #*(h)**(-1/3)
+            
+            arcsec = R_500/scale
+
+            pixelrad = arcsec/0.492
+            
+            ld = cosmo.luminosity_distance(z)
+            
+            self.CD = Label(master,text=('Critical density at z:   {0:.3e}'.format(pc_z),'Mo/Mpc^3'))
+            self.CD.pack()
+            self.CM = Label(master,text=('Cluster Mass within R_500:   {0:.3e}'.format(M_500),'Mo'))
+            self.CM.pack()
+            self.R = Label(master,text=('R_500: {0:.3e}'.format(R_500),'Mpc'))
+            self.R.pack()
+            self.a = Label(master,text=(arcsec,'Arcsec'))
+            self.a.pack()
+            self.PR = Label(master, text=('Pixel radius : {0:.3e}'.format(pixelrad)))
+            self.PR.pack()
+            
+            self.ld = Label(master, text=('Luminosity Distance : {0:.3e}'.format(ld)))
+            self.ld.pack()
+        if method =='clear':
+            self.CD.pack_forget()
+            self.CM.pack_forget()
+            self.R.pack_forget()
+            self.a.pack_forget()
+            self.PR.pack_forget()
+            self.ld.pack_forget()
+            
+            
+           
+    
+    def validate(self, new_text):
+        if not new_text: # the field is being cleared
+             self.entered_number = 0
+             return True
+        try:
+             self.entered_number = float(new_text)
+             return True
+        except ValueError:
+             return False
+
+root = Tk()
+one = GUI(root)
+root.mainloop()
